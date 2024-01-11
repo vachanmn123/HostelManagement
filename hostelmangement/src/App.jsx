ccaf1135496d61d57a130a8d8176d320d00772bf
Vachan MN <vachanmn123@gmail.com>
2024-01-11 11:06:12 +0530

    Initial

---
+++ hostelmangement/src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./components/Navbar";
+// import Sidebar from "./components/Sidebar";
+
+export default function App() {
+  return (
+    <div className="h-[100vh] bg-">
+      <Navbar />
+      {/* <Sidebar  /> */}
+    </div>
+  );
+}
